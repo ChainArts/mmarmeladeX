00b719789213aba822f340cde89b8ef9f58b11f5
tobytaken <tobiassteinhaeusler@gmail.com>
2024-03-08 19:31:48 +0100

    Does not work

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { KeyboardControls } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
+import Ecctrl from "ecctrl";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+  const keyboardMap = [
+    { name: "forward", keys: ["ArrowUp", "KeyW"] },
+    { name: "backward", keys: ["ArrowDown", "KeyS"] },
+    { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+    { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+    { name: "jump", keys: ["Space"] },
+    { name: "run", keys: ["Shift"] },
+    // Optional animation key map
+    { name: "action1", keys: ["1"] },
+    { name: "action2", keys: ["2"] },
+    { name: "action3", keys: ["3"] },
+    { name: "action4", keys: ["KeyF"] },
+  ];
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <>
+      <Canvas>
+        <Physics>
+          <KeyboardControls map={keyboardMap}>
+            <Ecctrl>
+              <RigidBody>
+                <mesh>
+                  <boxGeometry args={[2, 2, 2]} />
+                  <meshStandardMaterial />
+                  <ambientLight intensity={0.1} />
+                  <directionalLight color="red" position={[0, 0, 5]} />
+                </mesh>
+              </RigidBody>
+            </Ecctrl>
+            <RigidBody type="fixed" rotation={[-Math.PI / 2, 0, 0]}>
+              <mesh>
+                <planeGeometry args={[10, 10]} />
+                <meshStandardMaterial />
+                <ambientLight intensity={0.1} />
+                <directionalLight color="white" position={[0, 0, 5]} />
+              </mesh>
+            </RigidBody>
+          </KeyboardControls>
+        </Physics>
+      </Canvas>
+    </>
   );
 }
 
