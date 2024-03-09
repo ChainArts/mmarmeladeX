47d2a4595497ae5b29a25fb35c805448792acc36
tobytaken <tobiassteinhaeusler@gmail.com>
2024-03-09 15:49:30 +0100

    Player movement, deltaTime

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -4,10 +4,14 @@ import styles from "./Player.module.css";
 
 import Turtle from "../../assets/Turtle.png";
 
+const MOVEMENT_SPEED = 140;
+
 const Player: FC = () => {
   const playerRef = useRef<HTMLDivElement | null>(null);
 
   let direction = 2;
+  let lastFrameTime = Date.now();
+  let deltaTime = 0;
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -34,33 +38,30 @@ const Player: FC = () => {
   });
 
   const handleMovement = () => {
-    setTimeout(() => {
-      console.log("run");
-      if (!playerRef.current) return;
-      if (direction === 0) {
-        playerRef.current.style.top = `${parseInt(playerRef.current?.style.top || "0") - 2}px`;
-        playerRef.current.style.transform = "rotate(0deg)";
-      }
-      if (direction === 1) {
-        playerRef.current.style.left = `${parseInt(playerRef.current?.style.left || "0") + 2}px`;
-        playerRef.current.style.transform = "rotate(90deg)";
-      }
-      if (direction === 2) {
-        playerRef.current.style.top = `${parseInt(playerRef.current?.style.top || "0") + 2}px`;
-        playerRef.current.style.transform = "rotate(180deg)";
-      }
-      if (direction === 3) {
-        playerRef.current.style.left = `${parseInt(playerRef.current?.style.left || "0") - 2}px`;
-        playerRef.current.style.transform = "rotate(270deg)";
-      }
+    deltaTime = (Date.now() - lastFrameTime) / 1000.0;
+    if (!playerRef.current) return;
+    if (direction === 0) {
+      playerRef.current.style.top = `${parseInt(playerRef.current?.style.top || "0") - MOVEMENT_SPEED * deltaTime}px`;
+      playerRef.current.style.transform = "rotate(0deg)";
+    }
+    if (direction === 1) {
+      playerRef.current.style.left = `${parseInt(playerRef.current?.style.left || "0") + MOVEMENT_SPEED * deltaTime}px`;
+      playerRef.current.style.transform = "rotate(90deg)";
+    }
+    if (direction === 2) {
+      playerRef.current.style.top = `${parseInt(playerRef.current?.style.top || "0") + MOVEMENT_SPEED * deltaTime}px`;
+      playerRef.current.style.transform = "rotate(180deg)";
+    }
+    if (direction === 3) {
+      playerRef.current.style.left = `${parseInt(playerRef.current?.style.left || "0") - MOVEMENT_SPEED * deltaTime}px`;
+      playerRef.current.style.transform = "rotate(270deg)";
+    }
+    lastFrameTime = Date.now() * 1.0;
+    // console.log(playerRef.current.getBoundingClientRect().x, playerRef.current.getBoundingClientRect().y);
 
-      // console.log(playerRef.current.getBoundingClientRect().x, playerRef.current.getBoundingClientRect().y);
-
-      handleMovement();
-    }, 25);
+    requestAnimationFrame(handleMovement);
   };
-
-  handleMovement();
+  requestAnimationFrame(handleMovement);
 
   return (
     <div className={styles.container} ref={playerRef}>
