8f17264622c6278192ce3154f0beae41d1f16a56
tobytaken <tobiassteinhaeusler@gmail.com>
2024-03-10 10:37:37 +0100

    Add exit button, icon to StartScreen

--- src/ui-components/StartScreen/StartScreen.tsx
+++ src/ui-components/StartScreen/StartScreen.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
+import { appWindow } from "@tauri-apps/api/window";
 
 import styles from "./StartScreen.module.css";
+
+import icon from "/assets/icon.png";
 
 interface StartScreenProps {
   setStart: (start: boolean) => void;
@@ -9,10 +12,16 @@ interface StartScreenProps {
 const StartScreen: FC<StartScreenProps> = ({ setStart }) => {
   return (
     <div className={styles.container}>
+      <img className={styles.icon} src={icon} alt="" />
       <h1 className={styles.heading}>Turtle.js</h1>
-      <button className={styles.button} onClick={() => setStart(true)}>
-        Start Game
-      </button>
+      <div className={styles["button-container"]}>
+        <button className={styles.button} onClick={() => setStart(true)}>
+          Start Game
+        </button>
+        <button className={styles.button} onClick={() => appWindow.close()}>
+          Exit Game
+        </button>
+      </div>
     </div>
   );
 };
